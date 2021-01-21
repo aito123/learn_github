9a99173281bea23af4489bec858c5788a0238823
aito123 <sotelog001@gmail.com>
2021-01-21 16:00:56 -0500

    editando el main.

--- real_learn_github.R
+++ real_learn_github.R
@@ -2,3 +2,8 @@
 a=1
 b=2
 c=3
+
+#Ahora se supone que estoy en el main master y los cambios que haga acá no deberían
+#afectar new1. Y ahora que pasa si hago el merge del main master al branch.
+
+#Peace
